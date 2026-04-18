8c59da320b62fd713034fdf06bf9894869265ca5
Rishaad Khan <radishranger123@gmail.com>
2026-04-18 13:48:28 -0400

    Did some error handling for quad_transfer_matrix, will complete later

--- quadrupole_sim/src/physics.rs
+++ quadrupole_sim/src/physics.rs
@@ -1,4 +1,5 @@
 #![allow(non_snake_case)]
+use anyhow::Result;
 use ndarray::{Array2, array};
 
 use crate::PROTON_MASS;
@@ -17,52 +18,50 @@ pub fn quad_transfer_matrix(
     g: f32,     // Field gradient
     L: f32,     // Effective length
     B_rho: f32, // The beam rigidity
-) -> (Array2<f32>, Array2<f32>) {
-    let mut k2 = g / B_rho; // The magnetic gradient
-    let mut M_f: Array2<f32> = Array2::default((2, 2)); // Focusing matrix
-    let mut M_d: Array2<f32> = Array2::default((2, 2)); // Defocusing matrix
+) -> Result<(Array2<f32>, Array2<f32>)> {
+    let k2 = g / B_rho; // The magnetic gradient
     let k = k2.abs().sqrt();
 
     if k2.abs() < 1e-9 {
         // Near-zero gradient: both planes are drifts
         let drift = drift_matrix(L);
-        return (drift.clone(), drift);
+        return Ok((array![[1.0, 0.0], [0.0, 1.0]], drift));
     }
 
     if k2 > 0.0 {
         // Focusing in x, Defocusing in y
-        M_f = array![
+        let M_f = array![
             [(L * k.sqrt()).cos(), (L * k.sqrt()).sin() / k.sqrt()],
             [-1.0 * (L * k.sqrt()).sin() * k.sqrt(), (L * k.sqrt()).cos()]
         ];
-        M_d = array![
+        let M_d = array![
             [(L * k.sqrt()).cosh(), (L * k.sqrt()).sinh() / k.sqrt()],
             [
                 -1.0 * (L * k.sqrt()).sinh() * k.sqrt(),
                 (L * k.sqrt()).cosh()
             ]
         ];
+
+        Ok((M_f, M_d))
     } else {
         // Defocusing in x, Focusing in y
-        M_f = array![
+        let M_f = array![
             [(L * k.sqrt()).cosh(), (L * k.sqrt()).sinh() / k.sqrt()],
             [
                 -1.0 * (L * k.sqrt()).sinh() * k.sqrt(),
                 (L * k.sqrt()).cosh()
             ]
         ];
-        M_d = array![
+        let M_d = array![
             [(L * k.sqrt()).cos(), (L * k.sqrt()).sin() / k.sqrt()],
             [-1.0 * (L * k.sqrt()).sin() * k.sqrt(), (L * k.sqrt()).cos()]
         ];
-    }
 
-    (M_f, M_d)
+        Ok((M_f, M_d))
+    }
 }
 
 /// Returns a drift matrix
 pub fn drift_matrix(L: f32) -> Array2<f32> {
-    array![[1.0,L], [0.0,1.0]]
+    array![[1.0, L], [0.0, 1.0]]
 }
-
-
