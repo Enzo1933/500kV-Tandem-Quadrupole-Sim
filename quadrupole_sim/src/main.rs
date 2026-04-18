cceeb2f26f2902bf1f8c506a9cdf0e39291a8dfc
Rishaad Khan <radishranger123@gmail.com>
2026-04-18 13:47:59 -0400

    Changed the return type of main.rs

--- quadrupole_sim/src/main.rs
+++ quadrupole_sim/src/main.rs
@@ -10,6 +10,6 @@ const C: f32 = 1.0; // Speed of light c = 1
 const IN_TO_M: f32 = 0.0254; // Conversion factor 
 const MM_TO_M: f32 = 1e-3; // Conversion factor
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::io::Result<()> {
+    Ok(())
 }
